56f219db01f69f7bec877645b640ee2b80785bd0
Abhishek <akumar074@gmail.com>
2017-07-03 14:05:51 +0530

    NEW

--- src/main/java/com/stackroute/swisit/documentparser/service/ConceptNetServiceImpl.java
+++ src/main/java/com/stackroute/swisit/documentparser/service/ConceptNetServiceImpl.java
@@ -1,8 +1,12 @@
 package com.stackroute.swisit.documentparser.service;
 
+import com.stackroute.swisit.documentparser.domain.Term;
+import com.stackroute.swisit.documentparser.repository.Neo4jParserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by user on 30/6/17.
@@ -10,7 +14,14 @@ import java.util.HashMap;
 @Service
 public class ConceptNetServiceImpl implements ConceptNetService {
 
-    public HashMap<String,HashMap<String,Integer>> createDocumentModel(){
+    @Autowired
+    Neo4jParserService neo4jParserService;
+
+    public HashMap<String,HashMap<String,Integer>> createDocumentModel(HashMap<String,String[]> input){
+        List<Term> termsList = neo4jParserService.getTerms();
+        for(int i=0;i<termsList.size();i++){
+
+        }
         return null;
     }
 }
